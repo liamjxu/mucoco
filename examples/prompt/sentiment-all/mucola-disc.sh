7e78440ec3b6596d5aa347bea84624232b6ee0a8
liamxu24 <liamxu24@gmail.com>
2023-05-14 18:51:23 -0700

    arg fix

--- examples/prompt/sentiment-all/mucola-disc.sh
+++ examples/prompt/sentiment-all/mucola-disc.sh
@@ -15,10 +15,9 @@ NEGATIVELABEL=0
 
 # EPSILONS=(-5 -4 -3 -2 -1 0)
 EPSILONS=(-2) # the constraint is  log(negative_prob) - log (positive_prob) < epsilon, where positive_prob is the desired label
-
 for EPSILON in "${EPSILONS[@]}"
 do
-    bash examples/prompt/constrained_sampling_mucola.sh sentiment-disc $OUTPUTDIR $MODELNAME run_and_evaluate 0.25 dotplusplus l2 0.0 constant legal 0 1 0.0 false 1 0.5 0.05 50 target 100 1.0 $POSITIVELABEL 2 50 $EPSILON false false 0.45 0.01 0 true
+    bash examples/prompt/constrained_sampling_mucola.sh sentiment-disc $OUTPUTDIR $MODELNAME run_and_evaluate 0.25 dotplusplus l2 0.0 constant legal 0 1 0.0 false 1 0.5 0.05 50 target 100 1.0 $POSITIVELABEL 2 50 $EPSILON false false 0.45 0.01 0 20 true
     #note: most arguments passed to constrained_sampling_mucola.sh are not used anymore
 done
 
